686f088f7e5e2cc1a507d6d1547d57f51b08f145
Amir Zaidi <azaidi@live.nl>
2020-01-11 13:55:58 +0100

    [SQUASHED] ReloadedLauncher: Add Icon Pack Support from Shade Launcher
    @DennySPb: port to A11, A12-L, A13
    
    Change-Id: Id81da397d5ef33577d6f53f349080c49a61591ab
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    -----
    
    Launcher3: Make icon pack support themed icons
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    Launcher3: Switch to icon pack theming preview
    
    Ref:
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/8155a4f3db5888b578aecb8904529a22f962968e
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/174d11c6fa88e85920ec078af506fc56ef3bccd4
    
    Tweaked to our usage
    
    Change-Id: Ieb7a62c8df1ac0024e1238fec14d4d3c2ed5d38b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    -----
    
    Launcher3: Remove repeated strings in app info
    
    Change-Id: I97fc8fc2277e8be912ea33d3f5e346eb72b95c47
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    -----
    
    Launcher3: Use proper icon provider in LauncherAppState
    
    Change-Id: I1521995a7d98c147e394cf2834b8e9bc93e74b26
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    Launcher: Bottomsheet: Don't make package source pref clickable
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    -----
    
    InfoBottomSheet: Fix NPE with AppTransitionManager
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I03d5490b62c063e85c21a6c90301dee24b9b1871
    
    -----
    
    Launcher3: Do not attempt to show promise icon when info is null
    
    @neobuddy89: Adapted for A12.
    
    Change-Id: I9b7a4f1923194657f7fd5b0ad75fc095a072086b
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: StarkDroid <gamerprince.exp@gmail.com>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -53,6 +53,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.annotation.XmlRes;
 import androidx.core.content.res.ResourcesCompat;
 
+import com.android.launcher3.customization.IconDatabase;
 import com.android.launcher3.icons.DotRenderer;
 import com.android.launcher3.model.DeviceGridState;
 import com.android.launcher3.provider.RestoreDbTask;
@@ -135,6 +136,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
     public int numFolderColumns;
     public float[] iconSize;
     public float[] iconTextSize;
+    public String iconPack;
     public int iconBitmapSize;
     public int fillResIconDpi;
     public @DeviceType int deviceType;
@@ -334,6 +336,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
             case KEY_SHOW_DRAWER_LABELS:
             case KEY_ICON_SIZE:
             case KEY_FONT_SIZE:
+            case IconDatabase.KEY_ICON_PACK:
                 onConfigChanged(mContext);
                 break;
         }
@@ -389,6 +392,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
         for (int i = 1; i < iconSize.length; i++) {
             maxIconSize = Math.max(maxIconSize, iconSize[i]);
         }
+        iconPack = IconDatabase.getGlobal(context);
         iconBitmapSize = ResourceUtils.pxFromDp(maxIconSize, metrics);
         fillResIconDpi = getLauncherIconDensity(iconBitmapSize);
 
@@ -491,7 +495,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
     private Object[] toModelState() {
         return new Object[]{
                 numColumns, numRows, numSearchContainerColumns, numDatabaseHotseatIcons,
-                iconBitmapSize, fillResIconDpi, numDatabaseAllAppsColumns, dbFile};
+                iconPack, iconBitmapSize, fillResIconDpi, numDatabaseAllAppsColumns, dbFile};
     }
 
     private void onConfigChanged(Context context) {
