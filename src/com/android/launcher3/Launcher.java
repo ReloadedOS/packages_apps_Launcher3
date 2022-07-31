dff55b6a486f94b2a3a91247c15f1e3f59a47006
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-31 23:43:51 +0530

    ReloadedLauncher: Add a toggle for smartspace
    Change-Id: I3b2681d51197642ba0957111db455937c0c376e8

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -408,6 +408,8 @@ public class Launcher extends StatefulActivity<LauncherState>
 
     private CellPosMapper mCellPosMapper = CellPosMapper.DEFAULT;
 
+    private boolean mSmartspaceEnabled;
+
     @Override
     @TargetApi(Build.VERSION_CODES.S)
     protected void onCreate(Bundle savedInstanceState) {
@@ -493,6 +495,8 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         // TODO: move the SearchConfig to SearchState when new LauncherState is created.
         mBaseSearchConfig = new BaseSearchConfig();
+
+        mSmartspaceEnabled = Utilities.showSmartspace(this);
 
         mAppWidgetManager = new WidgetManagerHelper(this);
         mAppWidgetHolder = createAppWidgetHolder();
@@ -2336,11 +2340,11 @@ public class Launcher extends StatefulActivity<LauncherState>
     @Override
     public void bindScreens(IntArray orderedScreenIds) {
         int firstScreenPosition = 0;
-        if (FeatureFlags.QSB_ON_FIRST_SCREEN &&
+        if (mSmartspaceEnabled &&
                 orderedScreenIds.indexOf(Workspace.FIRST_SCREEN_ID) != firstScreenPosition) {
             orderedScreenIds.removeValue(Workspace.FIRST_SCREEN_ID);
             orderedScreenIds.add(firstScreenPosition, Workspace.FIRST_SCREEN_ID);
-        } else if (!FeatureFlags.QSB_ON_FIRST_SCREEN && orderedScreenIds.isEmpty()) {
+        } else if (!mSmartspaceEnabled && orderedScreenIds.isEmpty()) {
             // If there are no screens, we need to have an empty screen
             mWorkspace.addExtraEmptyScreens();
         }
@@ -2364,7 +2368,7 @@ public class Launcher extends StatefulActivity<LauncherState>
         int count = orderedScreenIds.size();
         for (int i = 0; i < count; i++) {
             int screenId = orderedScreenIds.get(i);
-            if (FeatureFlags.QSB_ON_FIRST_SCREEN && screenId == Workspace.FIRST_SCREEN_ID) {
+            if (mSmartspaceEnabled && screenId == Workspace.FIRST_SCREEN_ID) {
                 // No need to bind the first screen, as its always bound.
                 continue;
             }
