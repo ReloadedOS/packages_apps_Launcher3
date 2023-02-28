bf0ed371116b3c9d507579a101d62985b030b37a
Shen Lin <shhenlin@gmail.com>
2023-02-28 19:00:24 +0800

    ReloadedLauncher: Fix a potential NPE when buildAnimationController is called
    Fixes: 271010395
    Test: atest
    Change-Id: I581ba867bca6f3c8468d6e12f722cbfdd27e33a1

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -772,6 +772,9 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
         if (!canCreateNewOrUpdateExistingLauncherTransitionController()) {
             return;
         }
+        if (mActivity == null) {
+            return;
+        }
         initTransitionEndpoints(mActivity.getDeviceProfile());
         mAnimationFactory.createActivityInterface(mTransitionDragLength);
     }
